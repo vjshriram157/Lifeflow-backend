01fa225dc0e26174d795c3c8b8a14d6e9addf8b9
vjshriram157 <vijay.shriram157@gmail.com>
2026-03-15 13:30:20 +0530

    Added latest database dump (15-03-2026)

---
+++ db/Dump20260315/bloodbank_appointments.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bloodbank
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointments`
+--
+
+DROP TABLE IF EXISTS `appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `donor_id` int NOT NULL,
+  `bank_id` int NOT NULL,
+  `appointment_time` timestamp NOT NULL,
+  `status` varchar(20) DEFAULT 'PENDING',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_appointment_donor` (`donor_id`),
+  KEY `fk_appointment_bank` (`bank_id`),
+  CONSTRAINT `fk_appointment_bank` FOREIGN KEY (`bank_id`) REFERENCES `blood_banks` (`id`),
+  CONSTRAINT `fk_appointment_donor` FOREIGN KEY (`donor_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointments`
+--
+
+LOCK TABLES `appointments` WRITE;
+/*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
+INSERT INTO `appointments` VALUES (2,5,1,'2026-02-10 19:21:38','COMPLETED','2026-02-10 19:21:38'),(8,8,1,'2026-02-06 10:54:00','COMPLETED','2026-02-13 06:54:15'),(9,8,2,'2026-02-07 06:54:00','COMPLETED','2026-02-13 06:54:30'),(10,8,1,'2026-02-04 06:54:00','COMPLETED','2026-02-13 06:54:54'),(11,4,3,'2024-01-01 04:30:00','COMPLETED','2026-02-13 08:16:15'),(12,4,3,'2024-01-01 04:30:00','COMPLETED','2026-02-13 08:16:27'),(13,4,1,'2024-01-01 04:30:00','PENDING','2026-02-13 08:16:41'),(14,5,2,'2026-02-27 22:20:00','COMPLETED','2026-02-13 08:20:19'),(15,4,2,'2024-01-01 04:30:00','COMPLETED','2026-02-13 08:21:52'),(16,4,3,'2024-01-01 04:30:00','COMPLETED','2026-02-13 08:22:04'),(17,11,2,'2026-02-20 08:26:00','COMPLETED','2026-02-13 08:26:25'),(18,4,1,'2024-01-01 04:30:00','PENDING','2026-03-05 05:26:23');
+/*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-15 13:10:38
